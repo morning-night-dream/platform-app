c0daac83163f33deaa778a5484fae0586e3baa78
takokun <takokun.778@gmail.com>
2023-02-25 22:47:15 +0900

    fix

--- internal/adapter/controller/sign.go
+++ internal/adapter/controller/sign.go
@@ -12,7 +12,7 @@ import (
 	"github.com/morning-night-dream/platform-app/pkg/openapi"
 )
 
-// (POST /v1/sign)
+// (GET /v1/sign)
 func (ctl *Controller) V1Sign(w http.ResponseWriter, r *http.Request, params openapi.V1SignParams) {
 	ctx := r.Context()
 
