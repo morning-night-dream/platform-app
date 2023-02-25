9106ef46a6f20f7efc6e05b805a501ef97a09b51
takokun <takokun.778@gmail.com>
2023-02-26 01:51:40 +0900

    feat sign

---
+++ cmd/sign/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/base64"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/deepmap/oapi-codegen/pkg/types"
+	"github.com/google/uuid"
+	"github.com/morning-night-dream/platform-app/pkg/openapi"
+)
+
+func main() {
+	url := "http://localhost:8082"
+
+	client, err := openapi.NewClient(url + "/api")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	email := types.Email(uuid.NewString() + "@example.com")
+
+	password := uuid.NewString()
+
+	// signup
+	if _, err := client.V1AuthSignUp(ctx, openapi.V1AuthSignUpJSONRequestBody{
+		Email:    email,
+		Password: password,
+	}); err != nil {
+		panic(err)
+	}
+
+	// secret
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+
+	pub := priv.Public()
+
+	log.Printf("pub key: %+v", pub)
+
+	bytes, err := json.Marshal(pub)
+	if err != nil {
+		panic(err)
+	}
+
+	pubstr := base64.StdEncoding.EncodeToString(bytes)
+
+	// signin
+	res, err := client.V1AuthSignIn(ctx, openapi.V1AuthSignInJSONRequestBody{
+		Email:     email,
+		Password:  password,
+		PublicKey: pubstr,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	client.Client = &http.Client{
+		Transport: NewCookieTransport(res.Cookies()),
+	}
+
+	// sign
+	code := "reichankawaii"
+
+	h := crypto.Hash.New(crypto.SHA256)
+	h.Write(([]byte)(code))
+	hashed := h.Sum(nil)
+
+	signed, err := rsa.SignPKCS1v15(rand.Reader, priv, crypto.SHA256, hashed)
+	if err != nil {
+		panic(err)
+	}
+
+	signature := base64.StdEncoding.EncodeToString(signed)
+
+	res, err = client.V1Sign(ctx, &openapi.V1SignParams{
+		Code:      code,
+		Signature: signature,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("%+v", res)
+}
+
+type CookieTransport struct {
+	Cookies   []*http.Cookie
+	Transport http.RoundTripper
+}
+
+func NewCookieTransport(
+	cookies []*http.Cookie,
+) *CookieTransport {
+
+	return &CookieTransport{
+		Cookies:   cookies,
+		Transport: http.DefaultTransport,
+	}
+}
+
+func (ct *CookieTransport) transport() http.RoundTripper {
+	return ct.Transport
+}
+
+func (ct *CookieTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	for _, cookie := range ct.Cookies {
+		req.AddCookie(cookie)
+	}
+
+	resp, err := ct.transport().RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, err
+}
--- internal/adapter/controller/sign.go
+++ internal/adapter/controller/sign.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"crypto"
 	"crypto/rsa"
-	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -51,33 +51,56 @@ func (ctl *Controller) V1Sign(w http.ResponseWriter, r *http.Request, params ope
 
 	log.GetLogCtx(ctx).Info(fmt.Sprintf("pubkey: %s", auth.PublicKey))
 
-	pub, err := x509.ParsePKIXPublicKey(auth.PublicKey)
-	if err != nil {
-		log.GetLogCtx(ctx).Warn("failed to parse public key", log.ErrorField(err))
+	// publicKey, err := x509.ParsePKCS1PublicKey(auth.PublicKey)
+	// if err != nil {
+	// 	log.GetLogCtx(ctx).Warn("failed to parse public key", log.ErrorField(err))
 
-		w.WriteHeader(http.StatusInternalServerError)
+	// 	w.WriteHeader(http.StatusInternalServerError)
 
+	// 	return
+	// }
+
+	// pub, err := x509.ParsePKIXPublicKey(auth.PublicKey)
+	// if err != nil {
+	// 	log.GetLogCtx(ctx).Warn("failed to parse public key", log.ErrorField(err))
+
+	// 	w.WriteHeader(http.StatusInternalServerError)
+
+	// 	return
+	// }
+
+	// log.GetLogCtx(ctx).Info(fmt.Sprintf("pubkey: %+v", pub))
+
+	// publicKey, ok := pub.(*rsa.PublicKey)
+	// if !ok {
+	// 	log.GetLogCtx(ctx).Warn("failed to parse public key", log.ErrorField(err))
+
+	// 	w.WriteHeader(http.StatusInternalServerError)
+
+	// 	return
+	// }
+
+	// log.GetLogCtx(ctx).Info(fmt.Sprintf("pubkey: %+v", publicKey))
+
+	var public *rsa.PublicKey
+	if err := json.Unmarshal(auth.PublicKey, &public); err != nil {
 		return
 	}
-
-	log.GetLogCtx(ctx).Info(fmt.Sprintf("pubkey: %+v", pub))
-
-	publicKey, ok := pub.(*rsa.PublicKey)
-	if !ok {
-		log.GetLogCtx(ctx).Warn("failed to parse public key", log.ErrorField(err))
-
-		w.WriteHeader(http.StatusInternalServerError)
-
-		return
-	}
-
-	log.GetLogCtx(ctx).Info(fmt.Sprintf("pubkey: %+v", publicKey))
 
 	h := crypto.Hash.New(crypto.SHA256)
 	h.Write([]byte(params.Code))
 	hashed := h.Sum(nil)
 
-	if err := rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hashed, []byte(params.Signature)); err != nil {
+	sig, err := base64.StdEncoding.DecodeString(params.Signature)
+	if err != nil {
+		log.GetLogCtx(ctx).Warn("failed to decode signature", log.ErrorField(err))
+
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	if err := rsa.VerifyPKCS1v15(public, crypto.SHA256, hashed, sig); err != nil {
 		log.GetLogCtx(ctx).Warn("failed to verify signature", log.ErrorField(err))
 
 		w.WriteHeader(http.StatusUnauthorized)
