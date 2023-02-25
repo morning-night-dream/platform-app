956b2c0cbf7c912d4097cf41eff6b54a0350fee5
takokun <takokun.778@gmail.com>
2023-02-25 21:23:53 +0900

    feat

--- internal/adapter/controller/sign.go
+++ internal/adapter/controller/sign.go
@@ -1,12 +1,41 @@
 package controller
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/morning-night-dream/platform-app/pkg/log"
 	"github.com/morning-night-dream/platform-app/pkg/openapi"
 )
 
 // (POST /v1/sign)
 func (ctl *Controller) V1Sign(w http.ResponseWriter, r *http.Request, params openapi.V1SignParams) {
+	ctx := r.Context()
 
+	sid, err := r.Cookie(sidKey)
+	if err != nil {
+		log.GetLogCtx(ctx).Warn("failed to get auth", log.ErrorField(err))
+
+		w.WriteHeader(http.StatusUnauthorized)
+
+		rs := openapi.UnauthorizedResponse{}
+
+		if err := json.NewEncoder(w).Encode(rs); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		return
+	}
+
+	pubkey, err := ctl.public.Get(ctx, sid.Value)
+	if err != nil {
+		log.GetLogCtx(ctx).Warn("failed to get auth", log.ErrorField(err))
+
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	log.GetLogCtx(ctx).Info(fmt.Sprintf("pubkey: %s", pubkey))
 }
