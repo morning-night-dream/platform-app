135264f53bf85b1200b734b1b44d7ce74466e11d
Shitomo <cadaeibf@outlook.com>
2023-03-18 22:53:03 +0900

    cors

--- cmd/app/api/main.go
+++ cmd/app/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/morning-night-dream/platform-app/internal/adapter/gateway"
@@ -58,7 +60,7 @@ func main() {
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedOrigins:   strings.Split(config.API.CorsAllowOrigins, ","),
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
