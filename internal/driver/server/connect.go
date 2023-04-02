62f8cb314bae81ae3f712b056bd1819d55ab722b
takokun <takokun.778@gmail.com>
2023-04-02 23:41:06 +0900

    feat

--- internal/driver/server/connect.go
+++ internal/driver/server/connect.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/bufbuild/connect-go"
+	grpchealth "github.com/bufbuild/connect-grpchealth-go"
 	"github.com/morning-night-dream/platform-app/internal/adapter/controller"
 	"github.com/morning-night-dream/platform-app/pkg/connect/article/v1/articlev1connect"
 	"github.com/morning-night-dream/platform-app/pkg/connect/health/v1/healthv1connect"
@@ -27,6 +28,8 @@ func NewConnectHandler(
 		NewRoute(userv1connect.NewUserServiceHandler(user, interceptor)),
 		NewRoute(versionv1connect.NewVersionServiceHandler(version, interceptor)),
 	).Mux()
+
+	mux.Handle(grpchealth.NewHandler(grpchealth.NewStaticChecker()))
 
 	return h2c.NewHandler(mux, &http2.Server{})
 }
